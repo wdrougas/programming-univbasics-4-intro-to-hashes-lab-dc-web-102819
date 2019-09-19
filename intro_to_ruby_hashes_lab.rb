b9ff58a780d84ac3586b6065df5c5e06a80fabe2
Will Drougas <wdrougas@gmail.com>
2019-09-19 17:05:15 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -23,6 +23,6 @@ def read_from_hash(hash, key)
 end
 
 def update_counting_hash(hash, key)
-  hash = {"apples" => 3, "carrots" => 4}
+  hash = {"apples" => 3}
   update_hash = (hash["apples"] += 1)
 end
